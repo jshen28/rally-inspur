9cae38ed049fd05e14196eb84d4b6b8bb71c5a05
jshen28 <yshxxsjt715@gmail.com>
2018-09-20 23:38:48 +0800

    add logic determining if successfully detached

--- rally_inspur/scenarios/neutron/neutron_ha.py
+++ rally_inspur/scenarios/neutron/neutron_ha.py
@@ -118,7 +118,8 @@ class NeutronHaTest(utils.NeutronScenario, nova_utils.NovaScenario):
 
         for attachment in attachments:
             nova.servers.interface_detach(server, attachment.port_id)
-            LOG.info('server %s status %s' % (server.id, getattr(nova.servers, 'get')(server).status))
+            serverng = getattr(nova.servers, 'get')(server)
+            LOG.info('server %s status %s, task %s' % (server.id, serverng.status, serverng.get('OS-EXT-STS:task_state')))
 
             import time
             time.sleep(10)
