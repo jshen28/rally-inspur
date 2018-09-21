ec7ead413ca45b65dfee9550c482aa8a9ef8d966
shenjiatong <shenjiatong@inspur.com>
2018-09-21 09:19:24 +0800

    add externa

--- rally_inspur/scenarios/keepalived/keepalived_ha.py
+++ rally_inspur/scenarios/keepalived/keepalived_ha.py
@@ -25,7 +25,7 @@ class KeepalivedHa(BasicNovaHa):
             "image": image,
             "flavor": flavor,
             "hosts": hosts,
-            "cmd": 'systemctl stop keepavlived',
+            "cmd": 'systemctl stop keepalived',
             'restore': 'systemctl start keepalived'
         })
         self._run(salt_api_uri=salt_api_url, salt_user_passwd=salt_passwd, **kwargs)
